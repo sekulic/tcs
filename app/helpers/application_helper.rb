d000deb8f8b00c86bd100848b6910ca5f95ae8b6
sekulic <sekulic.radosav@yahoo.com>
2015-04-20 19:16:32 +0200

    add page active helpers for navabar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,5 +18,26 @@ module ApplicationHelper
     else
       return false
     end
-  end     
+  end
+  def is_active_page?(link_path)
+    if current_page?(link_path)
+      "active"
+    else
+      ""
+    end
+  end 
+  def is_active_page_home?
+    if current_page?(root_path) && request.env['ORIGINAL_FULLPATH'] == "/"
+      "active"
+    else
+      ""
+    end
+  end    
+  def active_page_category(cat_id)
+    if request.env['ORIGINAL_FULLPATH'] == "/?category_id=#{cat_id}"
+      "active"
+    else
+      ""
+    end
+  end         
 end
